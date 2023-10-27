b8873774905bdc090d074e5e11db7c8104b2ed14
Fabricio <contact@fabricioguardia.com>
2023-10-27 05:26:39 -0500

    pm2 file update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,22 +1,20 @@
 module.exports = {
   apps : [{
-    script: 'index.js',
-    watch: '.'
-  }, {
-    script: './service-worker/',
-    watch: ['./service-worker']
+    script: 'npm start'
   }],
 
   deploy : {
     production : {
-      user : 'SSH_USERNAME',
-      host : 'SSH_HOSTMACHINE',
+      key  : 'API.pem',
+      user : 'ubuntu',
+      host : '54.84.85.196',
       ref  : 'origin/master',
-      repo : 'GIT_REPOSITORY',
-      path : 'DESTINATION_PATH',
+      repo : 'git@github.com:codecallogic/absolutePrivateInvestigationLLC.git',
+      path : '/home/ubuntu/client',
       'pre-deploy-local': '',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
-      'pre-setup': ''
+      'post-deploy' : 'source ~/.nvm/nvm.sh && npm install && npm run build && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': '',
+      'ssh_options': 'ForwardAgent=yes'
     }
   }
 };
