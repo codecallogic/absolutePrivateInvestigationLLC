5b8af71797e34446ac10e6afcc7cb13de2b9c8fa
Fabricio <contact@fabricioguardia.com>
2023-10-27 13:05:08 -0500

    next config file update

--- next.config.js
+++ next.config.js
@@ -1,37 +1,32 @@
 const isProd = process.env.NODE_ENV === 'production'
 
-module.exports = {
-  reactStrictMode: true,
-  assetPrefix: isProd ? 'https://catsus.calstatela.edu' : undefined,
-  publicRuntimeConfig: {
-    APP_NAME: 'CEAS',
-    API: 'http://localhost:3001/api',
-    PRODUCTION: false,
-    DOMAIN: 'http://localhost:3000',
-    PUBLIC_FILES: 'http://localhost:3001/files/storage',
-    SOCKET: 'http://localhost:3001'
-  },
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
-  }
-}
-
-// const isProd = process.env.NODE_ENV === 'production'
-
 // module.exports = {
+//   reactStrictMode: true,
+//   assetPrefix: isProd ? 'https://catsus.calstatela.edu' : undefined,
+//   publicRuntimeConfig: {
+//     APP_NAME: 'API',
+//     API: 'http://localhost:3001/api',
+//     PRODUCTION: false,
+//     DOMAIN: 'http://localhost:3000',
+//     PUBLIC_FILES: 'http://localhost:3001/files/storage',
+//     SOCKET: 'http://localhost:3001'
+//   },
 //   eslint: {
 //     ignoreDuringBuilds: true,
-//   },
-//   reactStrictMode: true,
-//   assetPrefix: isProd ? 'http://54.84.85.196' : undefined,
-//   publicRuntimeConfig: {
-//     APP_NAME: 'Catsus',
-//     API: 'http://54.84.85.196/api',
-//     PRODUCTION: true,
-//     DOMAIN: 'http://54.84.85.196',
-//     PUBLIC_FILES: 'http://54.84.85.196/files/storage',
-//     SOCKET: 'http://54.84.85.196:3001'
 //   }
 // }
 
+module.exports = {
+  reactStrictMode: true,
+  publicRuntimeConfig: {
+    APP_NAME: 'API',
+    API: 'http://54.84.85.196:3001/api',
+    PRODUCTION: false,
+    DOMAIN: 'http://54.84.85.196:3000',
+    PUBLIC_FILES: 'http://54.84.85.196:3001/files/storage',
+    SOCKET: 'http://54.84.85.196:3001'
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  }
+}
